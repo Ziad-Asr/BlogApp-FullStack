c223c2c3c0baa77b1f5d2a67c9f8f01bf0ce86b1
Ziad <ZiadElsayedAsr@gmail.com>
2023-09-30 11:52:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./pages/Home/Home";
+import Settings from "./pages/Settings/Settings";
 import SinglePostPage from "./pages/SinglePost/SinglePostPage";
+import Write from "./pages/Write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <SinglePostPage />
+      <Settings />
     </>
   );
 }
