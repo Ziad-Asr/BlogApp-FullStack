8f79af1c6d33370c3d76bfe0f007192c798fa19e
mostafa-elnady <mostafa.elnady49@gmail.com>
2023-10-01 15:58:49 +0300

    update

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
-
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require('./routes/auth')
+
 dotenv.config();
+app.use(express.json()) // To enable sending json data on postman
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/", (req, res) => {
-  console.log("This is main URL.");
-});
+app.use("/api/auth", authRoute); // to enable auth route
 
 app.listen("3000", () => {
   console.log("Backend is running.");
